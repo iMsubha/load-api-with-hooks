621d9654c346bd5473e1e2bcb65ee25b33ce163a
Your Name <subha.fairuz@gmail.com>
2021-02-27 00:46:40 +0600

    commit

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -12,7 +12,8 @@ function User() {
     const displayUser = {
         display: 'grid',
         gridTemplateColumns: 'repeat(4, 1fr)',
-        gridGap: '10px'
+        gridGap: '10px',
+        margin: '40px'
     }
     return (
 
@@ -22,7 +23,7 @@ function User() {
                 <p>Count: {count}</p>
                 <button onClick={() => count > 0 ? setCount(count - 1) : setCount(0)}>Minus</button>
             </div>
-
+            <h3>JSON placeholder API</h3>
             <div style={displayUser}>
                 {
                     users.map(user => <Userdetails key={user.id} name={user.name} email={user.email} street={user.address.street} />)
